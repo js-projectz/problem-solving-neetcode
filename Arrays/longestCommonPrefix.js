996c50e9ca345f99e1372069ce971af8314d44db
Meganathan <meganathan212003@gmail.com>
2025-02-05 21:50:22 +0530

    feat(LongestCommonprefix): solve the today sum

---
+++ Arrays/longestCommonPrefix.js
@@ -0,0 +1,28 @@
+/**
+ * Here the problem is to find the
+ *  longest common prefix
+ * 
+ * @author meganathan
+ * @param {string[]} strs
+ * @return {string}
+ * 
+ */
+
+var longestCommonPrefix = function (strs) {
+
+    if (!strs.length) return '';
+
+    let prefix = strs[0];
+
+    for(let i = 1; i < strs.length; i++) {
+        while(strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.substring(0, prefix.length - 1);
+        }
+    }
+
+    return prefix;
+
+};
+
+const strs = ["flower", "flow", "flight"];
+console.log(longestCommonPrefix(strs)); // "fl"
