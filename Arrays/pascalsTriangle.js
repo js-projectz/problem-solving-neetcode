64335e2de374bc246d82f3a4e2063c08aaead9d2
Meganathan <meganathan212003@gmail.com>
2025-02-07 22:19:40 +0530

    feat(pascalTriangle): complete the pascal triangle

---
+++ Arrays/pascalsTriangle.js
@@ -0,0 +1,25 @@
+/**
+ * Here the problem is to find the
+ * Pascal's Triangle
+ * 
+ * @param {number} numRows
+ * @return {number[][]}
+ * 
+ * @author meganathan
+ * 
+ */
+
+
+var generate = function (numRows) {
+
+    if (numRows === 0) return [];
+
+    let result = [[1]];
+
+    for (let i = 1; i < result.length; i++) {
+
+        
+    }
+
+
+};
