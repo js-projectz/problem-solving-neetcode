77febbd26f70623c1383c17e4bb7d3c29f29fd16
Meganathan <meganathan212003@gmail.com>
2025-02-26 23:51:41 +0530

    feat(findpivot): solve the today sum find the pivot number

---
+++ Arrays/findPivotIndex.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function (nums) {
+
+    /**
+     * Input: nums = [1,2,3]
+Output: -1
+Explanation:
+There is no index that satisfies the conditions in the problem statement.
+     */
+
+    if (nums.length === 0) return 0;
+
+    let totalSum = nums.reduce((a, b) => a + b, 0);
+    var leftSum = 0;
+
+
+    for (let i = 0; i < nums.length; i++) {
+
+        var rightSum = totalSum - leftSum - nums[i]; // 6 - 1 - 2 = 3
+        // console.log('right-sum', rightSum);
+
+
+        if (leftSum === rightSum) {
+            return i; // found the pivot
+        }
+
+        leftSum = leftSum + nums[i];
+        // console.log('left-sum', leftSum);
+        // this code is update the next number in left
+    }
+
+
+    return -1;
+
+};
+
+const arr = [1, 2, 3];
+console.log(pivotIndex(arr));
