d3ac0759aa7cbad3838a5ed68c73fa629440b327
Meganathan <meganathan212003@gmail.com>
2025-03-07 21:02:12 +0530

    feat(arr): solve today arr problems

---
+++ Arrays/maxLengthBetweenEqualCharacters.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxLengthBetweenEqualCharacters = function (s) {
+
+    if (!s.length) return 0;
+
+
+    let max = -1;
+
+    for (let i = 0; i < s.length; i++) {
+        let firstIndex = s.indexOf(s[i]);
+        let lastIndex = s.lastIndexOf(s[i]);
+
+        console.log('firsindex', firstIndex);
+        console.log('lastindex', lastIndex);
+        console.log('max', max);
+
+        if (firstIndex !== lastIndex) {
+            max = Math.max(max, lastIndex - firstIndex - 1);
+        }
+    }
+
+    return max;
+};
+
+console.log(maxLengthBetweenEqualCharacters("abca"));
