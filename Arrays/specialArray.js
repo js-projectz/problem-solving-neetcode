271a77f87098601be841ad9bbb99e0bd543af5e9
Meganathan <meganathan212003@gmail.com>
2025-03-13 22:48:20 +0530

    feat(arr): sovle today problem

---
+++ Arrays/specialArray.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var specialArray = function (nums) {
+    nums.sort((a, b) => a - b);
+    let n = nums.length;
+
+    for (let x = 1; x <= n; x++) {
+        let count = nums.filter(num => num >= x).length; 
+        if (count === x) return x;
+    }
+
+    return -1;
+};
+
+
+console.log(specialArray([3, 5]));
