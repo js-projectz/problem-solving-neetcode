61a346510e743431fbf47aa568ba7a67f625dc28
meganathan2003 <meganathan212003@gmail.com>
2026-01-27 13:29:18 +0530

    alter the code

--- Matrix/columnWise.js
+++ Matrix/columnWise.js
@@ -11,7 +11,7 @@ const matrixColumnWise = (matrix) => {
 
     for (let i = 0; i < matrix.length; i++) {
         for (let j = 0; j < matrix[i].length; j++) {
-            result.push(matrix[j][i]); // 0 1 , 1 ,0
+            result.push(matrix[j][i]); // 0 1 , 1 ,0    
         }
     }
 
