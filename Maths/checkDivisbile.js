1cf4ae9200ae9322864a41716ad2f58082f82980
meganathan2003 <meganathan212003@gmail.com>
2026-02-05 13:24:23 +0530

    solve the problem

---
+++ Maths/checkDivisbile.js
@@ -0,0 +1,12 @@
+
+const checkDivisible = (userInput) => {
+
+    if (userInput.length === 0) return 'no';
+
+    const number = parseInt(userInput[0]);
+
+    const res = number % 7 === 0 ? 'yes' : 'no';
+
+    console.log(res);
+};
+checkDivisible(["14"]);
