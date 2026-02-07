7b1d25b1cd0c4a6f246f590e5c3306d13410a4fa
meganathan2003 <meganathan212003@gmail.com>
2026-02-07 11:15:44 +0530

    modify the code

--- Maths/middleEleModify.js
+++ Maths/middleEleModify.js
@@ -3,20 +3,13 @@ const middleEleModify = (userInput) => {
 //     convert the arr to str
     const str = userInput[0].split("");
     let mid = Math.floor(str.length / 2);
-
-    console.log("mid ", mid);
-
     // if user input is even
     if (str.length % 2 === 0) {
         // find the mid-value
-        str[mid - 1] = "*"
-        str[mid] = "*"
-        console.log(str.join(""));
-    } else {
-        str[mid] = "*"
-        console.log(str.join(""));
+        str[mid - 1] = "*";
     }
-
+    str[mid] = "*"
+    console.log(str.join(""));
 };
 
 middleEleModify(["sadsad"])
