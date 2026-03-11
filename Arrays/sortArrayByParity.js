73bce8fe71a3907f99e6dc5d7e199e7d1170aa99
meganathan2003 <meganathan212003@gmail.com>
2026-03-11 13:14:48 +0530

    solve today problem

--- Arrays/sortArrayByParity.js
+++ Arrays/sortArrayByParity.js
@@ -7,7 +7,22 @@ var sortArrayByParity = function (nums) {
     if (!nums) return [0];
 
 
-    // TODO : Need to complete the code
+    // two pointer approach
+    let left = 0;
+    let right = nums.length - 1;
 
+    while (left < right) {
+
+        while (left < right && nums[left] % 2 === 0) left++;
+        while (left < right && nums[right] % 2 === 1) right--;
+
+        [nums[left], nums[right]] = [nums[right], nums[left]];
+        left++, right--;
+    }
+
+    return nums;
 
 };
+
+
+console.log(sortArrayByParity([3, 1, 2, 4]));
