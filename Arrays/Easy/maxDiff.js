6e19dd919b6731e4aa6f240668a4561c52956d0f
meganathan2003 <meganathan212003@gmail.com>
2026-03-17 22:18:18 +0530

    add the new folder and solve the today problem

---
+++ Arrays/Easy/maxDiff.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    maxDifference(s) {
+        
+        let maxDiff = 0;
+        let map = new Map();
+
+        
+
+        
+        
+    }
+}
