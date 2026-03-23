e7c2c51bf0cbfd0176a17d00e27a71a981871a2e
meganathan2003 <meganathan212003@gmail.com>
2026-03-23 12:59:10 +0530

    solve the rotate sum

---
+++ Arrays/Medium/rotateArr.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+
+    k = k % nums.length;
+    nums.splice(0, 0, ...nums.splice(nums.length - k, k));
+
+    return nums;
+
+};
+
+console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3));
