35e2de64814d33bca84cd6647d6564ca8da11410
meganathan2003 <meganathan212003@gmail.com>
2026-03-25 23:30:21 +0530

    solve today sums

--- Arrays/Medium/threeSumClosest.js
+++ Arrays/Medium/threeSumClosest.js
@@ -1,1 +1,23 @@
+/**
+ * 
+ * @param {} nums 
+ * @param {} target 
+ * @returns 
+ */
 
+function threeSumClosest(nums, target) {
+    let closestSum = Infinity;
+    for (let i = 0; i < nums.length - 2; i++) {
+        for (let j = i + 1; j < nums.length - 1; j++) {
+            for (let k = j + 1; k < nums.length; k++) {
+                let currentSum = nums[i] + nums[j] + nums[k];
+                if (Math.abs(target - currentSum) < Math.abs(target - closestSum)) {
+                    closestSum = currentSum;
+                }
+            }
+        }
+    }
+    return closestSum;
+}
+
+console.log(threeSumClosest([-1, 2, 1, -4], 1));
