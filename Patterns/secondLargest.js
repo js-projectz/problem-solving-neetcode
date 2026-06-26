3365f2ecb721f589853669dbdd90609fe45e0778
meganathan2003 <meganathan212003@gmail.com>
2026-06-26 23:47:51 +0530

    change the logic and code

--- Patterns/secondLargest.js
+++ Patterns/secondLargest.js
@@ -7,19 +7,20 @@
 
 function secondLargest(arr) {
 
-    let max = arr[0];
-    let secondMax = -Infinity;
+    let largest = arr[0];
+    let secondLargest = arr[0];
 
     for (let i = 1; i < arr.length; i++) {
-
-        if (arr[i] > max) {
-            secondMax = max;
-            max = arr[i];
-        } else if (arr[i] > secondMax) {
-            secondMax = arr[i];
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        } else if (arr[i] > secondLargest) {
+            secondLargest = arr[i];
         }
     }
-    return secondMax;
+
+    return secondLargest;
+
 };
 
 console.log(secondLargest([1, 2, 3, 4, 5])); // Output: 4
