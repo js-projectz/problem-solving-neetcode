737ea5173d6ffd914e1927c4f15b7533c317eb07
Meganathan <109018167+meganathan2003@users.noreply.github.com>
2026-07-02 23:45:34 +0530

    sovle today problem

---
+++ Arrays/Easy/removeDuplicates.js
@@ -0,0 +1,21 @@
+/**
+ * Here the problem i solve is to remove duplicates from an array
+ * 
+ * @author Firstname Lastname
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+
+var removeDuplicates = function (nums) {
+    let i = 0;
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[i] !== nums[j]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+};
+
+console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
