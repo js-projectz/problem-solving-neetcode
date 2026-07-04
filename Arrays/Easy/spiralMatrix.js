e89f7669cc6319620b564243e6ab9d53d13a06b0
Meganathan <109018167+meganathan2003@users.noreply.github.com>
2026-07-04 23:37:13 +0530

    solve today problem

---
+++ Arrays/Easy/spiralMatrix.js
@@ -0,0 +1,46 @@
+/**
+ * Here the problem i solve is to print the spiral matrix 
+ */
+
+
+var spiralOrder = function (matrix) {
+    let result = [];
+    let top = 0;
+    let bottom = matrix.length - 1;
+    let left = 0;
+    let right = matrix[0].length - 1;
+
+    while (top <= bottom && left <= right) {
+        for (let i = left; i <= right; i++) {
+            result.push(matrix[top][i]);
+        }
+        top++;
+
+        for (let i = top; i <= bottom; i++) {
+            result.push(matrix[i][right]);
+        }
+        right--;
+
+        if (top <= bottom) {
+            for (let i = right; i >= left; i--) {
+                result.push(matrix[bottom][i]);
+            }
+            bottom--;
+        }
+
+        if (left <= right) {
+            for (let i = bottom; i >= top; i--) {
+                result.push(matrix[i][left]);
+            }
+            left++;
+        }
+    }
+
+    return result;
+};
+
+console.log(spiralOrder([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]));
